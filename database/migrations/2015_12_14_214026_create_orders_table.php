9d8116a64f5c9619054f2fe4bc59ee24cad41893
Marcelo Borges <mborgesmartins@gmail.com>
2016-03-12 18:39:06 -0300

    corrige migration order references to users

--- database/migrations/2015_12_14_214026_create_orders_table.php
+++ database/migrations/2015_12_14_214026_create_orders_table.php
@@ -15,7 +15,7 @@ class CreateOrdersTable extends Migration
         Schema::create('order', function (Blueprint $table) {
             $table->increments('id');
             $table->integer('user_id')->unsigned();
-            $table->foreign('user_id')->references('id')->on('user');
+            $table->foreign('user_id')->references('id')->on('users');
             $table->decimal('total', 8 , 2);
             $table->smallInteger('status')->default(0);
             $table->timestamps();
