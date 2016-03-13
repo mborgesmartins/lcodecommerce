ee1383cf0e69211e4ff55695ed2324779561b014
Marcelo Borges <mborgesmartins@gmail.com>
2016-03-12 23:01:02 -0300

    correção migração products

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -15,7 +15,7 @@ class DatabaseSeeder extends Seeder
         Model::unguard();
         $this->command->info('Unguarding models');
         Model::unguard();
-        DB::statement('SET FOREIGN_KEY_CHECKS=0;');
+        //DB::statement('SET FOREIGN_KEY_CHECKS=0;');
 
         $tables = [
             'product_tag',
@@ -36,7 +36,7 @@ class DatabaseSeeder extends Seeder
 
 // Call all the seeders
 
-        DB::statement('SET FOREIGN_KEY_CHECKS=1;');
+        //DB::statement('SET FOREIGN_KEY_CHECKS=1;');
 
 
         // $this->call(UserTableSeeder::class);
